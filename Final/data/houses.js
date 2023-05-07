016bb79e20bb7c1b6700cbea4b0df790f075a04c
Sadam Mohamed Usman <sadamtpt@gmail.com>
2023-05-06 23:23:23 -0400

    implemented search feature and address fields to house creation

--- Final/data/houses.js
+++ Final/data/houses.js
@@ -10,26 +10,29 @@ const create = async (accommodationData,emailAddress) => {
   roomCategory=roomCategory.trim().toLowerCase();
   gender=gender.trim().toLowerCase();
   city=city.trim().toLowerCase();
-  
-  if(!roomType || !roomCategory || !gender || !city || !state || !rent || !description){
+  state=state.trim();
+  address=address.trim();
+  description=description.trim();
+
+  if(!roomType || !roomCategory || !gender || !city || !state || !rent || !description || !address){
         throw "Enter all the fields";
   }
       
   if(roomType.trim().length==0 || roomCategory.trim().length==0 || gender.trim().length==0 || city.trim().length==0 || 
-            state.trim().length==0 || rent.trim().length==0 || description.trim().length==0){
+            state.trim().length==0 || rent.trim().length==0 || description.trim().length==0 || address.trim().length==0){
   
           throw "The entered field values should not be empty or contain white spaces";
   }
   
-  if(!roomType=="1bhk" || !roomType=="2bhk" || !roomType=="3bhk"){
+  if(!((roomType=="1bhk") ||(roomType=="2bhk") ||(roomType=="3bhk"))){
         throw "The room type needs to be 1BHK,2BHK,3BHK";
   }
       
-  if(!roomCategory=="private" || !roomCategory=="shared"){
+  if(!((roomCategory=="private") || (roomCategory=="shared"))){
         throw "The room category needs to be either Private or Shared";
   }
   
-  if(!gender=="male" || !gender=="female" || !gender=="any"){
+  if(!((gender=="male") || (gender=="female") || (gender=="any"))){
         throw "The gender needs to be either Male,Female or Any";
   }
   
@@ -39,16 +42,32 @@ const create = async (accommodationData,emailAddress) => {
         throw "Enter numerical values for the rent field";
   }
   
+  if(rentCheck<=0){
+    throw "Dont't enter values less than or equal to zero"
+  }
+
   let statesList=['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
   
   if(!statesList.includes(state)){
         throw "Do not enter the state out of the states list";
   }
+  let descriptionWords=description.trim().split(/\s+/).length;
 
+    let addressWords=address.trim().split(/\s+/).length;
+
+    if(descriptionWords.length>500){
+      throw "Description should be less than 500 words";
+    }
+
+    if(addressWords.length>30){
+      throw "Address should be less than 30 words"
+    }
   
   const newAccomodation = {
     roomType: roomType,
     roomCategory: roomCategory,
+    gender:gender,
+    address:address,
     city: city,
     state: state,
     rent: rent,
@@ -191,10 +210,95 @@ const update = async (id, updatedAccommodation) => {
   return await housesCollection.findOne({ _id: new ObjectId(id) });
 };
 
+
+const searchAccommodation=async(
+  roomType,
+  roomCategory,
+  gender,
+  city,
+  state  
+)=>{
+
+  if(!(roomType) || !(roomCategory)){
+      throw "Enter the values for room type and category ";
+  }
+
+  if(!gender){
+      throw "Enter the value for gender "
+  }
+
+  if(!state){
+      throw "Enter the value for state"
+  }
+  if(!city){
+    throw "Enter the value for city";
+  }
+  if(roomType.trim().length==0){
+      throw "Error room type should not contain empty spaces"
+  }
+
+  if(roomCategory.trim().length==0){
+      throw "Error room category should not contain empty spaces"
+  }
+
+  if(gender.trim().length==0){
+      throw "Error gender should not contain empty spaces"
+  }
+
+  if(state.trim().length==0){
+      throw "Error state should not contain empty spaces"
+  }
+  roomType=roomType.trim().toLowerCase();
+  roomCategory=roomCategory.trim().toLowerCase();
+  gender=gender.trim().toLowerCase();
+  city=city.trim().toLowerCase();
+  state=state.trim();
+
+  let statesList=['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+
+  if(!statesList.includes(state)){
+    throw "Do not enter the state out of the states list";
+  }
+  
+  if(!((roomType=="1bhk") || (roomType=="2bhk") || (roomType=="3bhk") || (roomType=="all"))){
+    throw "The room type for search needs to be 1BHK,2BHK,3BHK or all";
+  }
+  
+  if(!((roomCategory=="private") || (roomCategory=="shared"))){
+    throw "The room category needs to be either Private or Shared";
+  }
+
+  if(!((gender=="male") || (gender=="female") || (gender=="any"))){
+    throw "The gender needs to be either Male,Female or Any";
+  }
+
+  const housesCollection=await houseCollection();
+
+  let resultSet=null;
+
+  if(roomType=="all"){
+    
+    resultSet=await housesCollection.find({roomCategory:roomCategory,gender:gender,state:state,city:city}).toArray();
+  }
+  else{
+    resultSet=await housesCollection.find({roomType:roomType,roomCategory:roomCategory,gender:gender,state:state,city:city}).toArray();
+
+  }
+
+
+  if(resultSet.length==0){
+      throw "could not find the search results";
+  }
+
+  return resultSet;
+}
+
+
 export { 
   create, 
   getById, 
   getAll, 
   remove,
-  update
+  update,
+  searchAccommodation
 };
